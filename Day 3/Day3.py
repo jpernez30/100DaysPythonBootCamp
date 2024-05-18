d216f54c27b4588a31b2a683c932a23b5d9488b2
joven-pernez <jovenpernezjr@gmail.com>
2024-05-18 11:39:44 +0900

    Day 3: if else statement intro

---
+++ Day 3/Day3.py
@@ -0,0 +1,9 @@
+# Day 3: if else statements , already known.
+
+print("Are you tall enougn to ride the rollercoaster?\n")
+height = int(input("What is your height?"))
+
+if height < 120:
+    print("Sorry you are not tall enough =(")
+else:
+    print("You may ride the!")
