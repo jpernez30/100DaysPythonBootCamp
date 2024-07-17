8a4a5c8b9c800102c2b6b95275235497ba039a5f
kasugai-joven-pernez <jovenpernezjr@gmail.com>
2024-07-17 09:47:54 +0900

    Updated day 59

--- Day 59/main.py
+++ Day 59/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
@@ -20,6 +21,22 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    SMTP_ADDRESS = "smtp.gmail.com"
+    EMAIL_ADDRESS = "jovensubacc@gmail.com"
+    EMAIL_PASSWORD = "bwmg xjqp uthz zwfa"
+    data = request.form
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL_ADDRESS,
+            to_addrs=EMAIL_ADDRESS,
+            msg=f'This is the header \n\n Name: {data["name"]} Email: {data["email"]} Phone: {data["phone"]}  \n\n Message: {data["message"]}'.encode("utf-8")
+        )
+    return "<h1>Successfully sent your message</h1>"
 
 
 @app.route("/post/<int:index>")
